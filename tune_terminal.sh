2b0c4f2b66ff94e6c33e4caf6a3d07d098c6d867
Federico Damián Camarda <fededamian@gmail.com>
2019-04-11 14:02:27 -0300

    Added needed commands check, repos folder parameter, and .zshrc backup if present.

--- tune_terminal.sh
+++ tune_terminal.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
+# Global Variables.
+readonly REPOS_FOLDER="$HOME/public-repos"
+readonly NEEDED_COMMANDS="brew git"
+
+check_commands()
+{
+  # Create a counter of missing commands.
+  local MISSING_COUNTER=0
+
+  # Iterate through all of the needed commands provided in the global variable.
+  for NEEDED_COMMAND in $NEEDED_COMMANDS; do
+ 
+    # Verify if command exists.
+    if ! hash "$NEEDED_COMMAND" >/dev/null 2>&1; then
+
+      # Inform user the command was not found and increase the counter of
+      # missing commands.
+      printf "Command not found in PATH: %s\n" "$NEEDED_COMMAND" >&2
+        
+      # Increase counter.
+      ((MISSING_COUNTER++))
+      
+    fi
+
+    done
+
+    # If any of the commands is missing, the script finishes with an error code.
+    if ((MISSING_COUNTER > 0)); then
+       
+      # Error Message.
+      printf "Minimum %d commands are missing in PATH, aborting\n" "$MISSING_COUNTER" >&2
+
+      # Exit with error.
+      exit 1
+
+    fi
+}
 
 install_zsh_and_plugins()
 {
@@ -70,25 +107,26 @@ install_colorls()
 configure_zsh()
 {
   # Make sure a directory for the repo exists.
-  mkdir -p $HOME/repos
+  mkdir -p "$REPOS_FOLDER"
 
   # If powerlevel9k repository is present, do a pull. Otherwise, a clone.
-  if [ -d "$HOME/repos/powerlevel9k" ]; then
+  if [ -d "$REPOS_FOLDER/powerlevel9k" ]; then
 
     # Pull Powerlevel9k repo.
-    git -C $HOME/repos/powerlevel9k pull
+    git -C $REPOS_FOLDER/powerlevel9k pull
 
   else
 
     # Clone Powerlevel9k repo.
-    git -C $HOME/repos/ clone https://github.com/bhilburn/powerlevel9k.git
+    git -C $REPOS_FOLDER clone https://github.com/bhilburn/powerlevel9k.git
 
   fi
 
   # Inform the user that his .zshrc file will be overwritten and ask for
   # confirmation.
   echo "WARNING: Do you want the .zshrc in your home directory to be replaced with"
-  echo "         the repo´s version (Only 'yes' will be accepted to approve)?"
+  echo "         the repo´s version? Previous version will be stored as .bak."
+  echo "         (Only 'yes' will be accepted to approve)?"
 
   # Request user input.
   echo ""
@@ -98,6 +136,14 @@ configure_zsh()
   # Only if the user answers 'yes', the file will be replaced. 
   if [[ "$OPT_INSTALL_ZSHRC" == "yes" ]]; then
     
+    # If there was a .zshrc file, it will be backed up.
+    if [[ -f "$HOME/.zshrc" ]]; then
+
+      # Move current .zshrc file to a backup file.
+      mv "$HOME/.zshrc" "$HOME/.zshrc.bak" 
+
+    fi
+
     # Copy the .zshrc file of the repo to the User´s Home directory.
     cp ./zshrc.example $HOME/.zshrc
 
@@ -111,6 +157,8 @@ configure_zsh()
 
 main()
 {
+  check_commands
+
   install_zsh_and_plugins
 
   set_zsh_as_default_shell
